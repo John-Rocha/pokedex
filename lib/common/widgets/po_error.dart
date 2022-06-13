80201a1962dc5acb4517b4e4da6a2c97a24cef66
Johnathan Rocha <johnathanrocha@gmail.com>
2022-06-13 00:42:52 -0300

    feat: refactoring loading and error pages

---
+++ lib/common/widgets/po_error.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PoError extends StatelessWidget {
+  const PoError({
+    Key? key,
+    this.error,
+  }) : super(key: key);
+
+  final String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          error ?? 'Ocorreu um erro! Ligue para o professor Carvalho',
+          style: const TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
